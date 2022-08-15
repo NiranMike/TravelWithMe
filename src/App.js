be002bf8b0759e52adb40a9e2e0ea378c716c144
Olaniran Micheal <olaniranmicheal2020@gmail.com>
2022-08-15 01:51:26 +0100

    deleted a video and added a search component

--- src/App.js
+++ src/App.js
@@ -1,12 +1,15 @@
 import Navbar from "./componets/Navbar";
 import Hero from './componets/Hero';
 import Destination from "./componets/Destination";
+import Search from "./componets/Search";
+
 function App() {
   return (
     <div>
         <Navbar />
         <Hero />
         <Destination /> 
+        <Search />
     </div>
   );
 }
---
+++ src/componets/Search.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Search = () => {
+  return (
+    <div className='max-w-[1240px] mx-auto grid lg:grid-cols-3 gap-4 py-16'>
+
+    </div>
+  )
+}
+
+export default Search
